0426ba6e1fa7943b9224f6eabfa84501928f2fce
22stevenwu <22stevenwu@gmail.com>
2025-01-04 00:08:27 -0500

    base.html & base.css

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
-def home(request):
-  return render(request, "home.html")
+def profile(request):
+  return render(request, "profile.html")
 
 def login_view(request):
   return render(request, "login.html")
