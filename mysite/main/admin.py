f58c2b76650bcf757a1ddebce835d09f7cdd2363
22stevenwu <22stevenwu@gmail.com>
2025-01-06 21:32:32 -0500

    Bill Detail Page and Bill Split Logic

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Bill
+from .models import Bill, BillSplit
 
-# Register the Bill model with the admin site
 class BillAdmin(admin.ModelAdmin):
-    list_display = ('name', 'total_amount', 'tip_percentage', 'created_by', 'created_at')  # Columns to display in the list view
-    search_fields = ('name', 'created_by__username')  # Allow searching by bill name and username of the creator
-    list_filter = ('created_at',)  # Filter bills by creation date
+    list_display = ('name', 'total_amount', 'tip_percentage', 'tax_amount', 'created_by', 'created_at')
 
-admin.site.register(Bill, BillAdmin)  # Register the model with the custom admin class
+class BillSplitAdmin(admin.ModelAdmin):
+    list_display = ('bill', 'participant_name', 'amount_spent', 'amount_owed')
+
+admin.site.register(Bill, BillAdmin)
+admin.site.register(BillSplit, BillSplitAdmin)
