cbe453fea8712aeae12d38b1abc689096c0fd69c
22stevenwu <22stevenwu@gmail.com>
2025-01-09 14:44:10 -0500

    login page, past bills page, and profile html

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -15,6 +15,8 @@ class Bill(models.Model):
     tax_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)  
     created_at = models.DateTimeField(auto_now_add=True)
+    is_current = models.BooleanField(default=True)
+    total_owed = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
 class BillSplit(models.Model):
     bill = models.ForeignKey(Bill, related_name="splits", on_delete=models.CASCADE)
